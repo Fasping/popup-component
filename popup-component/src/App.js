20ce3f87960e174d5eae3487f23a7128dd4d02c0
Fernando Cases <nandocasesgarcia@gmail.com>
2022-11-18 09:23:47 +0100

    Build and done popup button, also build and done timed popup using useEffect hook

--- popup-component/src/App.js
+++ popup-component/src/App.js
@@ -1,16 +1,41 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Popup from './components/Popup';
 
 function App() {
+
+  const [buttonPopup, setButtonPopup] = useState(false)
+  const [timedPopup, setTimedPopup] = useState(false);
+  
+
+  const popupShow = () => {
+    setButtonPopup(true);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 3000);
+  },[])
+  
   return (
     <div className="App">
       <main>
         <h1>Pop Up Component</h1>
         <br />
         <br />
-        <button>Open PopUp</button>
-        <Popup trigger={false}>My Pop up </Popup>
+        <button onClick={popupShow}>Open PopUp</button>
       </main>
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>My Pop up</h3>
+        <p>This is my button triggered popup</p>
+      </Popup>
+
+      <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+        <h3>My Timed Popup</h3>
+        <p>This is my timed triggered popup</p>
+      </Popup>
     </div>
   );
 }
